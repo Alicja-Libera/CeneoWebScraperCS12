ec61d582d465d76c2fbd804fd073f2ef83202888
Alicja-Libera <ala.libera@gmail.com>
2025-06-09 17:35:04 +0200

    09/06/2025 - AL

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,33 @@
 from flask import render_template, redirect, request, url_for
-from app import app 
+from app import app
 from app.forms import ProductIdForm
-from app.models import Products 
-
+from app.model import Product
 
 @app.route("/")
 def index():
-    return render_template("Index.html")
+    return render_template("index.html")
 
 @app.route("/extract")
 def display_form():
     form = ProductIdForm()
     return render_template("extract.html", form=form)
 
-@app.route("/extract", methods = ['POST'] )
+@app.route("/extract", methods=['POST'])
 def extract():
     form = ProductIdForm(request.form)
     if form.validate():
-        product_id = form.product_id.data 
-        product = 
-        return redirect(url_for ("product", product_id = product_id))
+        product_id = form.product_id.data
+        product = Product(product_id)
+        product.extract_name()
+        product.extract_opinions()
+        product.calculate_stats()
+        product.generate_charts()
+        print(product)
+        product.save_opinions()
+        product.save_info()
+        return redirect(url_for('product', product_id=product_id))
     else:
-        return render_template("extract.html", form=form)
+         return render_template("extract.html", form=form)
 
 
 @app.route("/product/<product_id>")
@@ -29,8 +35,8 @@ def product(product_id):
     return render_template("product.html", product_id=product_id)
 
 @app.route("/charts/<product_id>")
-def charts():
-    return render_template("charts.html")
+def charts(product_id):
+    return render_template("charts.html", product_id=product_id)
 
 @app.route("/products")
 def products():
@@ -39,4 +45,3 @@ def products():
 @app.route("/about")
 def about():
     return render_template("about.html")
-
